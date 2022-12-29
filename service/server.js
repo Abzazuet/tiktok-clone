c5bb6f13d9867563898453aed0caa5754a1f3904
Abel Zazueta <abzazuet@amazon.com>
2022-12-29 15:40:59 -0800

    Deployed

--- service/server.js
+++ service/server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import Data from "./seeds.js";
 import Videos from "./dbModel.js";
 const app = express(); // Instatiate our server
-const port = 9000; // Select port
+const port = process.env.PORT || 9000; // Port or own port
 
 // app config
 
@@ -11,8 +11,8 @@ const port = 9000; // Select port
 app.use(express.json());
 // Whenever we receive a request, accept all the request
 app.use((req, res, next) => {
-  res.setHeaders("Access-Control-Allow-Origin", "*"),
-    res.setHeaders("Access-Control-Allow-Headers", "*"),
+  res.setHeader("Access-Control-Allow-Origin", "*"),
+    res.setHeader("Access-Control-Allow-Headers", "*"),
     next();
 });
 // db config
